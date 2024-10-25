bb5c0147b374b5942f081195d17b26e6bac82d78
Deanlad98 <sba24448@student.cct.ie>
2024-10-25 13:48:24 +0100

    This is the start of my first Java project wish me luck! -Added file reading functionality using BufferedReader and FileReader for customer data. -I added all the files i needed

---
+++ Customerlist/src/customerlist/Main.java
@@ -0,0 +1,12 @@
+package customerlist;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+    // Starting point of Java application
+public static void main(String[] args) {
+     // I am adding the file path to the customer.txt file
+     String filePath = "customer.txt";
+    
