4e6b83b220f712ae9c4737589291c9795d7583f3
Deanlad98 <sba24448@student.cct.ie>
2024-10-25 13:52:36 +0100

    Added BufferedReader to read data from the customer file with error handling.

--- Customerlist/src/customerlist/Main.java
+++ Customerlist/src/customerlist/Main.java
@@ -10,3 +10,7 @@ public static void main(String[] args) {
      // I am adding the file path to the customer.txt file
      String filePath = "customer.txt";
     
+ //I am using BufferedReader to read data from the customer file
+try (BufferedReader reader = new BufferedReader(new FilePath(filepath))){
+     String line;
+     
