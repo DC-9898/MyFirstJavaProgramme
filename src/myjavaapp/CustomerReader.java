b49d0d641d6bc297e3191b08a5241665c279f7b2
Deanlad98 <sba24448@student.cct.ie>
2024-10-26 20:15:49 +0100

    Im using BufferedReader to read the lines perfectly

--- src/myjavaapp/CustomerReader.java
+++ src/myjavaapp/CustomerReader.java
@@ -11,7 +11,11 @@ public class CustomerReader {
 //Method to read customers from a file
 public List<String[]> readCustomers(String filePath){
     List<String[]> customers = new ArrayList<>();
-}    
+    
+    // Im using BufferedReader to read the file line by line
+    try (BufferedReader = new BufferedReader(new FileReader(filePath))) {
+        
+    }    
   
     
 }
