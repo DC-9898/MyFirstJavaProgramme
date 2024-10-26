4c1fcb19c62f786ed7ee133e29302f61d63c027b
Deanlad98 <sba24448@student.cct.ie>
2024-10-26 19:59:53 +0100

    Added CustomerReader class to read customer data from file using BufferedReader.

--- src/myjavaapp/CustomerReader.java
+++ src/myjavaapp/CustomerReader.java
@@ -1,7 +1,14 @@
 // Author-Dean Carroll
 package myjavaapp;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+         
 public class CustomerReader {
+    
     
     
 }
