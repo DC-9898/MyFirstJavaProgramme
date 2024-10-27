6bfd416e0066492cc888e4fdee6b14c64577ac73
Deanlad98 <sba24448@student.cct.ie>
2024-10-27 17:12:06 +0000

    I completed my CustomerReader file

--- src/myjavaapp/CustomerReader.java
+++ src/myjavaapp/CustomerReader.java
@@ -8,9 +8,9 @@ import java.util.ArrayList;
 import java.util.List;
  
 public class CustomerReader {
-//Method to read customers from a file
+    //Method to read customers from a file
 public List<String[]> readCustomers(String filePath){
-    List<String[]> customers = new ArrayList<>();
+       List<String[]> customers = new ArrayList<>();
     
     // Im using BufferedReader to read the file line by line
 try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
@@ -18,25 +18,25 @@ try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
         //Looping through each line until theres no more data
 while ((line = reader.readLine()) != null) {
          
-        // Splitting each line into customer data sections
+    // Splitting each line into customer data sections
     String[] customerData = line.split(",");
             
-        // Making sure there are exactly 4 pieces of info (Name, Amount, Discount level. Year)
+    // Making sure there are exactly 4 pieces of info (Name, Amount, Discount level. Year)
 if (customerData.length == 4){
         // Adding valid customer data to the list
-    customers.add(customerData); 
-}else {
-        // If the line dosent have 4 pieces it will show an error
-    System.out.println("Invalid data format in line: " + line);
-  }
+    customers.add(customerData); }
+else {
+    // If the line dosent have 4 pieces it will show an error
+    System.out.println("Invalid data format in line: " + line); }
  } 
-}catch (IOException e) {
-        // Handling any file reading errors 
+}
+catch (IOException e) {
+    // Handling any file reading errors 
     System.out.println("Error reading the file: " + e.getMessage()); 
 }
-        // This will return the list of customer data
+    // This will return the list of customer data
 return customers;
  
  }
 }
-          
+    
